5af0bbc6d670763b8cccb62acac86ca1da24162e
AnthonySoares <anthonyjeansores@gmail.com>
2016-01-11 15:45:35 -0300

    Update teste.java

--- teste.java
+++ teste.java
@@ -1,9 +1,10 @@
 public class teste {
 
-public static void main(String[] args)
-{
+public static void main(String[] args) {
 
 System.out.println("Teste");
+System.out.println("Estamos Lascados!");
+
 }
 
 
